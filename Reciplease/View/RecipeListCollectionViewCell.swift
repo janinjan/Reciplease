3b963e6cba64016b03a5f06a98069d6738cab081
janinjan <janin.culhaoglu@gmail.com>
2019-07-20 19:54:46 +0200

    Improved convertUrlToImage method

--- Reciplease/View/RecipeListCollectionViewCell.swift
+++ Reciplease/View/RecipeListCollectionViewCell.swift
@@ -21,16 +21,16 @@ class RecipeListCollectionViewCell: UICollectionViewCell {
     // MARK: - Methods
     // Add shadows and border to cells
     func addShadowsToCell() {
-    contentView.layer.cornerRadius = 8.0
-    contentView.layer.borderWidth = 1.0
-    contentView.layer.borderColor = UIColor.clear.cgColor
-    contentView.layer.masksToBounds = false
-    layer.shadowColor = UIColor.gray.cgColor
-    layer.shadowOffset = CGSize(width: 0, height: 1.0)
-    layer.shadowRadius = 4.0
-    layer.shadowOpacity = 1.0
-    layer.masksToBounds = false
-    layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
+        contentView.layer.cornerRadius = 8.0
+        contentView.layer.borderWidth = 1.0
+        contentView.layer.borderColor = UIColor.clear.cgColor
+        contentView.layer.masksToBounds = false
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        layer.shadowRadius = 4.0
+        layer.shadowOpacity = 1.0
+        layer.masksToBounds = false
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: contentView.layer.cornerRadius).cgPath
     }
 
     // Add contrast to recipe image
@@ -60,14 +60,11 @@ class RecipeListCollectionViewCell: UICollectionViewCell {
 
     // Convert String URL to UIImage
     func convertUrlToImage(_ imageUrl: String) {
-        if let url = URL(string: imageUrl) {
-            if let data = NSData(contentsOf: url) {
-                guard let image = UIImage(data: data as Data) else {  // Convert url to image
-                return
-                }
-                let constratedImage = increaseContrast(image)
-                recipeListImageView.image = constratedImage // Add contrast to recipe image
-            }
+        guard let url = URL(string: imageUrl) else { return }
+        if let data = try? Data(contentsOf: url) {
+            guard let image = UIImage(data: data as Data) else { return }  // Convert url to image
+            let constratedImage = self.increaseContrast(image)
+            self.recipeListImageView.image = constratedImage // Add contrast to recipe image
         }
     }
 }
